5c9f095bee03112ae8eb5aedd13a6d66709e0957
travis-ci <travis@travis-ci.org>
2016-05-26 04:15:51 +0000

    ApiGen (Travis Build : 24  - Branch : master)

--- apigen/master/elementlist.js
+++ apigen/master/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Charcoal\\Factory\\AbstractFactory"],["c","Charcoal\\Factory\\FactoryInterface"],["c","Charcoal\\Factory\\GenericFactory"],["c","Charcoal\\Factory\\MapFactory"],["c","Charcoal\\Factory\\ResolverFactory"]];
+ApiGen.elements = [["c","Charcoal\\Factory\\AbstractFactory"],["c","Charcoal\\Factory\\FactoryInterface"],["c","Charcoal\\Factory\\GenericFactory"],["c","Charcoal\\Factory\\GenericResolver"],["c","Charcoal\\Factory\\MapFactory"],["c","Charcoal\\Factory\\ResolverFactory"]];
